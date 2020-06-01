3ed145f65cedbbaa8b40460c5b100fdb147ae2e9
ggasnier <23357977+ggasnier@users.noreply.github.com>
2020-06-01 16:51:15 +0200

    Page Annuaire

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var autoprefixer = require("autoprefixer");
-var postcss = require("gulp-postcss");
-var cssnano = require("cssnano"); // minifies CSS
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var cssnano = require('cssnano'); // minifies CSS
 
 var browsersList = [
-  "> 1%",
-  "last 2 versions",
-  "IE >= 10",
-  "Edge >= 16",
-  "Chrome >= 60",
-  "Firefox >= 50",
-  "Firefox ESR",
-  "Safari >= 10",
-  "ios_saf >= 10",
-  "Android >= 5",
+  '> 1%',
+  'last 2 versions',
+  'IE >= 10',
+  'Edge >= 16',
+  'Chrome >= 60',
+  'Firefox >= 50',
+  'Firefox ESR',
+  'Safari >= 10',
+  'ios_saf >= 10',
+  'Android >= 5',
 ];
 
 var pluginsProd = [
@@ -27,15 +27,15 @@ var pluginsProd = [
   //cssnano()
 ];
 
-gulp.task("sass", function () {
+gulp.task('sass', function () {
   return gulp
-    .src("assets/sass/style.scss")
+    .src('assets/sass/style.scss')
     .pipe(sass())
     .pipe(postcss(pluginsProd))
-    .pipe(gulp.dest("assets/css"));
+    .pipe(gulp.dest('assets/css'));
 });
 
 // Création de la tâche principale de surveillance
-gulp.task("watch", function () {
-  gulp.watch("assets/sass/*.scss", gulp.series("sass"));
+gulp.task('watch', function () {
+  gulp.watch('assets/sass/*.scss', gulp.series('sass'));
 });
